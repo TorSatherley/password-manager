f8ecafa7e972c6c0781c8b2995ce475f38351ca9
TorSatherley <torsatherley@hotmail.co.uk>
2025-02-19 12:41:11 +0000

    still working on util tests

--- test/test_manager_utils.py
+++ test/test_manager_utils.py
@@ -1,9 +1,8 @@
 from utils.manager_utils import get_user_choice
 import pytest
-from unittest import mock, patch
+from unittest.mock import Mock, patch
 
 class TestGetUserChoice:
-
     def test_function_returns_choice_for_valid_choice(self):
         with patch.object(__builtins__, 'input', lambda: 'l'):
             assert get_user_choice() == 'list'
