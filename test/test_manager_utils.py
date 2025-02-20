223f88ffd074e7b98b75224dd86c5c9ebd87106d
TorSatherley <torsatherley@hotmail.co.uk>
2025-02-20 10:45:54 +0000

    completes the util function get_user_choice with tests

--- test/test_manager_utils.py
+++ test/test_manager_utils.py
@@ -5,9 +5,19 @@ from unittest import TestCase
 
 
 
-class TestGetUserChoice(TestCase):
+class TestGetUserChoice():
 
-    @patch('utils.manager_utils.get_user_choice.user_choice', 'l')
-    def test_function_returns_choice_for_valid_choice(self):
+    @pytest.mark.parametrize('user_input', ['l', 'e', 'r', 'd', 'x'])
+    def test_function_returns_choice_for_valid_choice(self, user_input):
+        with patch('builtins.input', return_value=user_input):
             result = get_user_choice()
-            self.assertEqual(result, 'l')
+            assert result == user_input
+
+    @pytest.mark.parametrize('user_input', ['a', 't', '!', ' ', 'X'])
+    def test_function_returns_choice_for_valid_choice(self, user_input, capsys):
+        with patch('builtins.input', return_value=user_input):
+            result = get_user_choice()
+            captured = capsys.readouterr()
+            assert captured.out == 'invalid input.\n'
+            assert result == 'invalid'
+            
--- utils/manager_utils.py
+++ utils/manager_utils.py
@@ -1,6 +1,10 @@
-
-
 def get_user_choice():
     user_choice = str(input("Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it: "))
-    return(user_choice)
+    allowed_list = ['e', 'r', 'd', 'l', 'x']
+    if user_choice in allowed_list:
+        return(user_choice)
+    else:
+        print('invalid input.')
+        return 'invalid'
+        
 
